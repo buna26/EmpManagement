700fe447121388c6f75b119c7a66729ba20ed9e8
unknown <riyadasr26@gmail.com>
2023-04-27 01:54:46 +0530

    Add employee details

---
+++ Employee.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+class EmpDetails{
+	private String firstname, lastname;
+	private long empID;
+	private String email;
+	private String designation;
+	
+	EmpDetails(String firstname, String lastname, String email, String designation){
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.empID = firstname.hashCode();
+		this.email = email;
+		this.designation = designation;
+	}	
+	
+	public void setFristName(String firstname) { this.firstname = firstname; }
+	public String getFirstName() { return firstname; }
+	
+	public void setLastName(String lastname) { this.lastname = lastname; }
+	public String getLastName() { return lastname; }
+	
+	public void setEmail(String email) { this.email = email; }
+	public String getEmail() { return email; }
+	
+	public void setDesignation(String designation) { this.designation = designation; }
+	public String getDesignation() { return designation; }
+
+	public boolean isValidEmail(String email){ 
+		if(email.indexOf('@')!= -1)
+			return true;
+		return false;
+	}
+	
+	public void info() {
+        System.out.println("Name : "+ firstname +" "+ lastname);
+		System.out.println("Employee ID : "+ empID);
+		if(!isValidEmail(email)){
+		    System.out.println("Email ID is not valid");
+		}else{
+		    System.out.println("Email ID : "+email);
+		}
+		System.out.println("Designation : " + designation);
+    }
+}
+
+class Employee{
+	public static void main(String args[]){
+		
+		EmpDetails emp = new EmpDetails("Riya", "Das", "riyadas@gmail.com", "Analyst");
+		
+		emp.info();
+		
+	}
+}
