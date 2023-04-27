791b7aa73a1a0e2c5cfea5f6de63f606dd0e46b9
unknown <riyadasr26@gmail.com>
2023-04-27 14:05:51 +0530

    Add employee details

--- Employee.java
+++ Employee.java
@@ -47,7 +47,7 @@ class EmpDetails{
 class Employee{
 	public static void main(String args[]){
 		
-		EmpDetails emp = new EmpDetails("Raktim", "Deka", "riyadas@gmail.com", "Analyst");
+		EmpDetails emp = new EmpDetails("Riya", "Das", "riyadas@gmail.com", "Analyst");
 		
 		emp.info();
 		
